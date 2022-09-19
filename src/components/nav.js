70916f72713dd12c8c36ab7a56ff8706375234ef
Divine <deevynobeten@gmail.com>
2022-09-19 19:54:20 +0100

    adding elements to the navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from "react";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+// import LightModeIcon from "@mui/icons-material/LightMode";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 
 const Nav = () => {
   return (
     <>
       <header>
-          <nav>
-            <img src="" alt="" />            
-            <div className="theme-toggle"></div>
-            <div className="cart"></div>
-          </nav> 
-      </header> 
+        <nav>
+          <h1>Nav Logo</h1>
+          {/* <img src="" alt="" /> */}
+          <div className="cart-toggle">
+            <div className="theme-toggle">
+              <DarkModeIcon />
+              <p>Dark Mode</p>
+            </div>
+            <div className="cart">
+              <ShoppingBagIcon />
+            </div>
+          </div>
+        </nav>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
