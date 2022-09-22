9acdd47266b3e6db436094eaae8ec9994e1d9c3a
Divine <deevynobeten@gmail.com>
2022-09-22 09:03:42 +0100

    created hero page and added images, made it responsive

--- src/components/mainShop.js
+++ src/components/mainShop.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import HeroImage from "../assets/heroimage.jpg";
+import HeroLandcape from "../assets/heroimageLandscape.jpg"
 
 const MainShop = () => {
   return (
-    <div className='testing'>
-        jump!!!
-    </div>
-  )
+    <>
+      <div className="hero">
+        <div className="hero-img">
+          <img src={HeroImage} alt="" className="hero-img portrait" />
+          <img src={HeroLandcape} alt="" className="hero-img landscape" />
+        </div>
+        <div className="overlay"></div>
+      </div>
+    </>
+  );
 }
 
 export default MainShop 
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Logo from "../assets/logo.png";
-import Cart from "../assets/cart.png"
-// import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import Cart from "../assets/cart.png";
+import User from "../assets/user.png"
 
 const Nav = () => {
   return (
     <>
       <header>
         <nav>
-          <img src={Logo} alt="Comfy Logo" height={80} />
-          <div className="cart-toggle">
+          {/* <div className="nav-links">
+            <ul>
+              <li>Shop</li>
+              <li>About Us</li>
+            </ul>
+          </div> */}
+          <img src={Logo} alt="Comfy Logo" height={60} />
+          <div className="cart-profile">
+            <div className="profile">
+              <img src={User} alt="Log in or Sign up" />
+            </div>
             <div className="cart">
               <img src={Cart} alt="go to cart" />
             </div>
