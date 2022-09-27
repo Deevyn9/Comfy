a9b783b2f8f3437553159bcf87a9eba3d42281dc
Divine <deevynobeten@gmail.com>
2022-09-27 17:47:17 +0100

    Created cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Deal1 from "../assets/Deals/deal 1.jpg";
 
 const Cart = () => {
   return (
@@ -8,6 +9,29 @@ const Cart = () => {
         <Link to="/shop">
           <button>Continue Shopping</button>
         </Link>
+      </div>
+      <div className="main-cart">
+        <div className="cart-item">
+          <div className="item-img">
+            <img src={Deal1} alt="" />
+          </div>
+          <div className="remove-name">
+            <div className="item-name">
+              <p>Three seater Couch and Two Armchair</p>
+            </div>
+            <div className="remove">remove</div>
+          </div>
+          <div className="quan-price">
+            <div className="quantity">
+              <div className="minus"> - </div>
+              <div className="count">2</div>
+              <div className="plus">+</div>
+            </div>
+            <div className="item-price">
+              <p>$1100</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Deal1 from "../assets/Deals/deal 1.jpg";
 
 const Shop = () => {
   return (
-    <div className='main__shop'>
+    <div className="main__shop">
       <div className="go-back">
         <Link to="/">
-            <button>Home</button>
+          <button>Home</button>
         </Link>
       </div>
+      <div className="shop-container">
+        <div className="rectangle">
+          <img src={Deal1} alt="" />
+          <div className="prod-details">
+            <div className="prod-name">
+              <h3>Three seater Couch and Armchair X2</h3>
+            </div>
+            <div className="prod-price">
+              <p>
+                $1100 <span className="old-price">$1400</span>
+              </p>
+            </div>
+          </div>
+          <button className="add-cart">Add to Cart</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Shop
